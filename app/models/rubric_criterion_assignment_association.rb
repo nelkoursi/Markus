f4d7dd7b447298488ef99564d8dbfad3ebdca7d8
Nora <nora@ubuntu.(none)>
2012-12-06 14:36:18 +0100

    Revert "-Suppression of the radio button (flexible/rubric criterion) when creating an assignment in form.html.erb"
    This reverts commit 15385a7544b10b61c48e84404becbe1da1268a83.

--- app/models/rubric_criterion_assignment_association.rb
+++ app/models/rubric_criterion_assignment_association.rb
@@ -1,3 +1,6 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
 class RubricCriterionAssignmentAssociation < ActiveRecord::Base
   
   belongs_to :assignment
